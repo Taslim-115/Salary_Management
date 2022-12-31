167513217c76957517d92f62c0598065ccf20610
Taslim-115 <116017458+Taslim-115@users.noreply.github.com>
2023-01-01 03:53:13 +0600

    Update login.h

--- Header/login.h
+++ Header/login.h
@@ -1,9 +1,6 @@
-
-
-
 void login(void)
 {
-    char USERNAME[50];
+    char USERNAME[40];
     char PASS[33];
 
     char username[100]; /*this is for user input*/
@@ -15,32 +12,35 @@ void login(void)
     ptr_u = fopen("username.txt", "r");
     ptr_p = fopen("pass.txt", "r");
 
-    fgets(USERNAME, 7, ptr_u);
-    fgets(PASS, 5, ptr_p);
+    char *status1 = fgets(USERNAME, U_SIZE, ptr_u);
+    while (status1 != NULL)
+    {
+
+        status1 = fgets(USERNAME, U_SIZE, ptr_u);
+    }
+    // fgets(USERNAME, 7, ptr_u);
+    // fgets(PASS, 5, ptr_p);
+    char *status = fgets(PASS, P_SIZE, ptr_p);
+    while (status != NULL)
+    {
+
+        status = fgets(PASS, P_SIZE, ptr_p);
+    }
     // puts(USERNAME);
     // puts(PASS);
     fclose(ptr_p);
     fclose(ptr_u);
 
-    
     printf("\v\v");
     printf("\t\t\t\t\t\t\t\t\t\tUsername : ");
     gets(username);
     printf("\t\t\t\t\t\t\t\t\t\tPassword : ");
     gets(pass);
     system("cls");
-    while (log == 0)
+
+    if ((strcmp(USERNAME, username) != 0) || (strcmp(PASS, pass) != 0))
     {
-        if ((strcmp(USERNAME, username) == 0) && (strcmp(PASS, pass) == 0))
-        {
-            log = 1;
-            break;
-        }
         printf("\t\t\t\t\t\t\t\t\t%sInvalid username or password%s\v\v", KRED, KNRM);
-        printf("\t\t\t\t\t\t\t\t\t\tUsername : ");
-        gets(username);
-        printf("\t\t\t\t\t\t\t\t\t\tPassword : ");
-        gets(pass);
-        system("cls");
+        login();
     }
 }
